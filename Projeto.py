54c7044257b54f4decfc40b5b4c1c18f2c0de613
Gabriel Zaiac <gz.gabrielzaiac@gmail.com>
2025-04-17 15:00:23 -0300

    feat: adjust to add data to a main multidimencional array like [[],[]]. Added 40-chars limit for name. Cpf lenght != 11 occuried an error. alignment data as a table. Add backup function, and I did minor refactoring

--- Projeto.py
+++ Projeto.py
@@ -7,6 +7,7 @@ import json
 import random
 import os
 import shutil
+import time
 
 
 # classe para cores de texto
@@ -62,8 +63,7 @@ def title():
 def selected(selec):
     def manage(aba):
         clear()
-
-        # print(aba)
+        aba -= 1
 
         # printar menu de operações + área escolhida + função para selecionar opção para gerenciamento
         def call():
@@ -78,7 +78,7 @@ def selected(selec):
                     0: "Menu principal"
                 }
 
-                print(f"{cor.blue}--> Menu de operações: {cor.green}{cor.underline}{options[selec]}{cor.reset}\n")
+                print(f"{cor.blue}--> Menu de operações: {cor.bold}{cor.green}{cor.underline}{options[selec]}{cor.reset}\n")
                 for i in operations:
                     print(f"{cor.pink}{i} - {operations[i]}")
                 print(cor.blue)
@@ -105,110 +105,148 @@ def selected(selec):
 
             # abrir arquivo para gerenciar + verificar se arquivo existe. + se não, ent crie um e escreva [] nele
             # se o arquivo for alterado por fora, de um jeito fora da sintaxe, então ele gera um arquivo de backup(do erro), coloca em backups e reescreve o json atual
+            #usei um numero 'aleatório' pq fiquei com preguiça de usar datetime
+            def bkup(e):
+                log = random.randint(100, 1000000)
+                os.makedirs("backups", exist_ok=True)
+                backup = f"backups/backupError_{log}.json" # seria interessante ver o histórico de mudanças que houve no arquivo.. tipo undo do vim
+                shutil.copy("data.json", backup)
+                clear()
+                print(f"{cor.red}erro: {e}{cor.cyan}\nbackup feito em {backup}{cor.red}")
+                sendData([[],[],[],[],[]])
+                exit(1)
+
             def openFile():
                 try:
                     with open(jsonPath, "r") as file:
                         global data
                         data = json.load(file)
                 except FileNotFoundError:
-                    sendData([])
+                    sendData([[],[],[],[],[]])
                     openFile()
                 except json.JSONDecodeError as e:
-                    #usei um numero 'aleatório' pq fiquei com preguiça de usar datetime
-                    log = random.randint(100, 1000000)
-                    os.makedirs("backups", exist_ok=True)
-                    backup = f"backups/backupError_{log}.json" # seria interessante ver o histórico de mudanças que houve no arquivo.. tipo undo do vim
-                    shutil.copy("data.json", backup)
-                    clear()
-                    print(f"{cor.red}erro: {e}{cor.cyan}\nbackup feito em {backup}{cor.reset}")
-                    sendData([])
-                    exit(1)
+                    bkup(e)
+
+            # apenas transformanda data de: [[n1],[n2]...] para [nx]
+            def dataRedirect():
+                try:
+                    global data
+                    data = list(data[aba])
+                except IndexError as e:
+                    bkup(e)
 
 
             # ler os dados do arquivo, futuramente vou adicionar uma array para diferentes listas das opções do menu
+            # eu poderia fzr mais simples.. porém eu não teria mais controle do espaçamento entre os elementos ent deixei assim mesmo
             def readFile():
                 openFile()
+                dataRedirect()
                 if data == []:
-                    print(f"{cor.bold}{cor.red}{data}")
+                    print(f"{cor.bold}{cor.red}lista vazia\n")
+                    call()
                 print(cor.faint, end="")
-                print("-> {0:<3}|{1:^6}|{2:>4}".format("id","nome","cpf"))
+                print("-> {0:<3}|{1:^8}|{2:>4}".format("id","nome","cpf"))
                 print(cor.reset, end="")
                 print(cor.yellow)
+                lenName = []
                 for i in data:
-                    items = list(i.values())
-                    print("> {0:<4} {1}\t{2}".format(items[0], items[1], items[2]))
+                    lenName.append(len(i["nome"]))
+                maxName =  max(lenName)
+                for i in data:
+                    itens = list(i.values())
+                    conta = 45 - len(i["nome"])
+                    non = ""
+                    print(f"{cor.yellow}> {itens[0]:<4}{itens[1]}{cor.faint}{non:.>{conta}}{cor.reset}{cor.yellow}{itens[2]}")
                 print(cor.reset)
-                print(f"{cor.faint}{cor.cyan}==============================={cor.reset}\n")
-
-
+                print(f"{cor.faint}{cor.cyan}{" end of list ":-^62}{cor.reset}\n")
 
             # função para inserir info no arquivo json
             # eu fiz para que o id sempre fosse maior que o ultimo id da lista, e mesmo se apagar qualquer valor o id vai ser diferente
             def writeFile():
                 try:
-                    nome = input(f"{cor.blue}insira seu nome: {cor.white}")
+                    nome = input(f"{cor.blue}insira seu nome (max. 45): {cor.white}")[:45].lower()
                     cpf = int(input(f"{cor.blue}insira o CPF do estudante ({cor.green}{cor.underline}apenas números{cor.reset}{cor.blue}): {cor.white}"))
-
+                    if len(str(cpf)) != 11:
+                        clear()
+                        print(f"{cor.red}cpf inválido: 11 digitos{cor.reset}")
+                        call()
+                    clear()
+                    print(f"{cor.green}adicionado!")
                     openFile()
+                    dataRedirect()
                     codigo = len(data)
                     if data:
-                        codigo = len(data) - 1
+                        codigo -= 1
                         pid = data[codigo]["id"]
                         codigo = pid + 1
                     info = {"id": codigo, "nome": nome, "cpf": cpf}
-                    data.append(info)
+                    openFile()
+                    data[aba].append(info)
                     sendData(data)
                 except ValueError:
                     print(f"{cor.red}informações inválidas :[")
                     call()
 
+            # função para atualizar lista do json :|
+            def updateInfo():
+                try:
+                    readFile()
+                    update = int(input(f"{cor.blue}digite o id de quem deseja alterar: {cor.white}"))
+                    openFile()
+                    dataRedirect()
+                    for i in range(len(data)):
+                        if update == data[i]["id"]:
+                            newName = input(f"\n{cor.blue}coloque um novo nome: {cor.white}").lower()
+                            newCpf = int(input(f"{cor.blue}escolha seu nove cpf: {cor.white}"))
+                            if len(str(newCpf)) != 11:
+                                clear()
+                                print(f"{cor.red}cpf inválido: 11 digitos{cor.reset}\n{cor.blue}")
+                                updateInfo()
+                            data[i]["nome"] = newName
+                            data[i]["cpf"] = newCpf
+                            newInfo = data[i]
+                            openFile()
+                            data[aba][i] = newInfo
+                            sendData(data)
+                            clear()
+                            print(f"{cor.green}alterado!")
+                            call()
+                    print(f"\n{cor.red}Esse id não existe")
+                except ValueError:
+                    print(f"\n{cor.red}coloque corretamente as informações")
 
             # função para excluir info da lista em json
             def excludeInfo():
                 try:
-                    pop = int(input(f"código de quem deseja remover: {cor.white}"))
+                    readFile()
+                    pop = int(input(f"{cor.blue}id de quem deseja remover: {cor.white}"))
                     openFile()
+                    dataRedirect()
                     for i in range(len(data)):
                         verId = data[i].get("id")
                         if pop == verId:
                             del data[i]
+                            newInfo = data
+                            openFile()
+                            data[aba] = newInfo
                             sendData(data)
+                            clear()
                             print(f"\n{cor.green}estudante excluido!")
                             call()
                     print(f"\n {cor.red}esse estudante não exite") # posso fazer no futuro para escolher a quem dizer que está sendo feito a execução ex: Estudante, Disciplinas
                 except ValueError:
                     print(f"{cor.red}escolha um valor válido")
 
-            # função para atualizar lista do json :|
-            def updateInfo():
-                try:
-                    update = int(input(f"digite o id do aluno que deseja alterar: {cor.white}"))
-                    openFile()
-                    for i in range(len(data)):
-                        if update == data[i]["id"]:
-                            newName = input(f"\n{cor.blue}coloque um novo nome: {cor.white}")
-                            newCpf = int(input(f"{cor.blue}escolha seu nove cpf: {cor.white}"))
-                            data[i]["nome"] = newName
-                            data[i]["cpf"] = newCpf
-                            sendData(data)
-                            print(f"{cor.green}estudante alterado com sucesso!")
-                            call()
-                    print(f"\n{cor.red}Esse estudante não existe")
-                except ValueError:
-                    print(f"\n{cor.red}coloque corretamente as informações")
-
             def searchInfo():
-                info = input(f"id, nome ou cpf: {cor.white}")
+                info = input(f"procurar por: {cor.white}")
                 openFile()
+                dataRedirect()
                 for i in data: # poderia usar 2 for, mas dae teria que fazer a mesma coisa em readFile :|
                     pid = i["id"]
                     nome = i["nome"]
                     cpf = i["cpf"]
                     if info in str((pid, nome, cpf)):
-                        print(f"\n{cor.yellow}> {pid} {nome} {cpf}{cor.reset}\n")
-                        break
-                    print(f"\n{cor.lightred}não foi possivel encontrar o estudante\n") #aqui também posso colocar!!
-                    break
+                        print(f"{cor.yellow}> {pid} {nome} {cpf}{cor.reset}")
 
             # match case para executar funções acima
             # no futuro posso fazer para mandar um valor para info da área.. ex: 1: estudante 2: disciplina, para verificar em qual array colocar as informações
@@ -235,19 +273,20 @@ def selected(selec):
         call()
 
 
-    # estrutura match case para verificar e executar as funcionalidades.
-    match selec:
-        case 0:
+    # função para verificar e executar as funcionalidades.
+    def selection(aba):
+        if selec >= 1 and selec <= 5:
+            manage(aba)
+
+        elif selec == 0:
             print("\nVocê fechou o aplicativo :(")
             exit(0)
 
-        case 1:
-            manage(selec)
+        else:
+            print(f"{cor.red}acho q vc não escolher a opção correta!")
+            exit(1)
+    selection(selec)
 
-        case 2 | 3 | 4 | 5:
-            clear()
-            print(f"{cor.red}EM DESENVOLVIMENTO\n")
-            main()
 
 # função para iniciar o código e verificar opções :P
 # vai dar ValueError se n selecionar uma opção listada. e o código vai reiniciar
